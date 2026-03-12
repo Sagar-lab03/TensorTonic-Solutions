d102f7ac6146789d19dde95751c019beecc40f77
Sagar-lab03 <85815988+Sagar-lab03@users.noreply.github.com>
2026-03-12 23:53:46 +0530

    Update solution for matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -6,4 +6,17 @@ def matrix_transpose(A):
     """
     # Write code here
     
-    return np.transpose(A)
+    row = len(A)
+    col = len(A[0])
+
+    temp = [n for row_ in A for n in row_]
+
+    res = np.zeros((col, row))
+
+    i = 0
+    for x in range(row):
+        for y in range(col):
+            res[y][x] = temp[i]
+            i += 1
+
+    return res
